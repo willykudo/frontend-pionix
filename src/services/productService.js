f1b404d5eba61c2b261a66226961fa958ad16b5e
willykudo <72055688+willykudo@users.noreply.github.com>
2025-02-14 23:34:46 +0700

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/products';
+const API_URL = `${process.env.REACT_APP_API_URL}/api/products`;
 
 const getProducts = async (token) => {
     const response = await axios.get(API_URL, {
