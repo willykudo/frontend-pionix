2c96a7c4b5cbe48c2a9e0f253faf6cab2575e6f5
willykudo <72055688+willykudo@users.noreply.github.com>
2025-02-14 23:37:31 +0700

    Update shiftService.js

--- src/services/shiftService.js
+++ src/services/shiftService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/shifts';  // Sesuaikan dengan URL backend Anda
+const API_URL = `${process.env.REACT_APP_API_URL}/api/shifts`;  
 
 // Mengambil semua shift
 const getShifts = async (token) => {
