cf8a7ffc4550b69ecb200f3721d4333b84ab1552
willykudo <72055688+willykudo@users.noreply.github.com>
2025-02-14 23:34:03 +0700

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/authService.js
 import axios from 'axios';
 
-const API_URL = '/api/auth';
+const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
 
 const register = async (userData) => {
     return await axios.post(`${API_URL}/register`, userData);
