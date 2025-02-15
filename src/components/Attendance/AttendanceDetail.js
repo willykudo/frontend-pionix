ce29d3fd7b9f194e2b42e8d2b19ed1a143298379
willykudo <72055688+willykudo@users.noreply.github.com>
2025-02-15 20:57:18 +0700

    Update AttendanceDetail.js

--- src/components/Attendance/AttendanceDetail.js
+++ src/components/Attendance/AttendanceDetail.js
@@ -78,8 +78,8 @@ const AttendanceDetail = ({ attendance, onClose, fetchAttendances }) => {
                 shiftStartTime: attendance.shiftStartTime ? moment(attendance.shiftStartTime).tz('Asia/Jakarta').format('YYYY-MM-DDTHH:mm') : '',
                 shiftEndTime: attendance.shiftEndTime ? moment(attendance.shiftEndTime).tz('Asia/Jakarta').format('YYYY-MM-DDTHH:mm') : ''
             });
-            setPreviewCheckInImage(`https://backend-pionix.onrender.com/uploads/${attendance.checkInImage}`);
-            setPreviewCheckOutImage(`https://backend-pionix.onrender.com/uploads/${attendance.checkOutImage}`);
+            setPreviewCheckInImage(`https://backend-pionix.onrender.com/${attendance.checkInImage}`);
+            setPreviewCheckOutImage(`https://backend-pionix.onrender.com/${attendance.checkOutImage}`);
         }
     }, [attendance]);
 
